065ac74f30e58b59d0aa4f81fa0a63e9c184bebd
t0nik0 <dantonov@gmail.com>
2015-10-22 18:23:16 +0300

    changed format

--- getlastdate.py
+++ getlastdate.py
@@ -1,17 +1,20 @@
-import time, logging, os
+import logging, os
 from suds.client import Client
+from time import tzset, localtime, strftime
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('suds.client').setLevel(logging.DEBUG)
 
 os.environ['TZ'] = 'Europe/Moscow'
 url = 'http://vigruzki.rkn.gov.ru/services/OperatorRequest/?wsdl'
-time.tzset()
+tzset()
 
 client = Client(url)
 result = client.service.getLastDumpDateEx()
 
-print "lastDumpDate \t\t = \t", time.strftime("%Y-%m-%d %H:%M:%S %Z",
-	time.localtime(result['lastDumpDate']/1000))
-print "lastDumpDateUrgently \t = \t", time.strftime("%Y-%m-%d %H:%M:%S %Z",
-	time.localtime(result['lastDumpDateUrgently']/1000))
+hl = "-" * 79 + "\n"
+str = hl + "| %-23s | %-23s | %-23s |\n" % ("CurrentTime", "LastDumpDate", "LastDumpDateUrgently")
+str = str + hl + "| " + strftime("%Y-%m-%d %H:%M:%S %Z", localtime()) + " | "
+str = str + strftime("%Y-%m-%d %H:%M:%S %Z", localtime(result['lastDumpDate']/1000)) + " | "
+str = str + strftime("%Y-%m-%d %H:%M:%S %Z", localtime(result['lastDumpDateUrgently']/1000)) + " |\n" + hl
+print str
